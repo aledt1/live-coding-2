5b11c8484082f2c7e89d6b22163ccbff20dc5862
alexanderDelgado <aledt2103@gmail.com>
2022-09-30 13:12:58 -0500

    chore | se añade ejercicio 2

--- starter-files/script.js
+++ starter-files/script.js
@@ -11,3 +11,29 @@ const API_ENDPOINT = 'https://yesno.wtf/api';
  * 5. Optional: add loading/error states
  *
  */
+
+ let promesa = fetch(API_ENDPOINT,{method: "GET"});
+
+ promesa.then((respuesta) =>{
+     respuesta.json().then((datos) =>{
+         console.log(datos);
+         console.log(typeof datos);
+         datos.forEach(ans => {
+             if (ans.answer == "yes"){
+                 keila.innerHTML += `<img src=${ans.image} height="500" style="border-radius:10px;margin-bottom:1rem;"/>
+                 <div style="padding:0.35rem 0.7rem;
+                     background: #94dbff;
+                     border-radius:10px;
+                     width:300px;
+                     text-align:center;
+                     font-weight:600;
+                     font-size:0.85rem">
+                 </div>`
+             }
+         });
+     }).catch((error) =>{
+         console.error(error);
+     });
+ }).catch((error) =>{
+     console.log("Error en la solicitud" + error);
+ });
